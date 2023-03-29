30da4b2bf25595ef2b3988250579710bc824fbfe
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-03-29 17:12:07 +0100

    add print() and pop() function

--- L1/PSD/stack1.c
+++ L1/PSD/stack1.c
@@ -1,14 +1,16 @@
 // Array implementaion of stacks. 
 
 #include <stdio.h> 
+#include <stdlib.h>
 #define MAX 4
 
 int stack_arr[MAX];
 int top = -1;  // indicates that the stack is empty.
 
-void push(int data);
+void push(int data); // function to push data in the stack.
+int pop(); // function to pop data from the stack. 
+void print(); // function to print the data int stack.
 
-// function to push data in stack.
 void push(int data) {
 
   if (top == MAX - 1) {
@@ -16,8 +18,32 @@ void push(int data) {
     return; // indicate the end of the function.
   }
 
-  top =+ 1;
+  top++;
   stack_arr[top] = data;
+}
+
+int pop() {
+  int value;
+  
+  if (top == -1) {
+    printf("Stack overflow");
+    exit(1);
+  }
+
+  value = stack_arr[top];
+  top--;
+  return value;
+ }
+
+void print() {
+  
+  if (top == -1) {
+    printf("Stack underflow");
+    return;
+  }
+
+  for (int i = top; i >= 0; i--)
+    printf("%d \n", stack_arr[i]);
 }
 
 int main() {
@@ -25,7 +51,12 @@ int main() {
   push(2);
   push(3);
   push(4);
+  
+  int data;
 
+  data = pop();
+  data = pop();
+  print();
 
   return 0;
 }
