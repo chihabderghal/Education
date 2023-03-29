cf10f628f98cd86bc8c1d7d06d110c558d831ec1
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-03-29 22:01:19 +0100

    write program-1

---
+++ L1/PSD/stack2.c
@@ -0,0 +1,45 @@
+// Program to impliment a stack in an array using stack_arr[0] as the top element.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 5
+
+int stack_arr[MAX];
+int top = -1;
+
+void push(int data);
+void print();
+
+void push(int data) {
+  
+  top++;
+  for (int i = top; i > 0; i--) 
+    stack_arr[i] = stack_arr[i-1];
+
+  stack_arr[0] = data;
+}
+
+void print() {
+
+  if (top == -1) {
+    printf("Underflow\n");
+    exit(1);
+  }
+
+  for (int i = 0; i <= top; i++)
+    printf("%d \n", stack_arr[i]);
+}
+
+int main() {
+
+  push(10);
+  push(20);
+  push(30);
+  push(40);
+  push(50);
+
+  print();
+
+  return 0;
+}
