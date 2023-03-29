e72fb2e61c825963fbc5dcc29615a82da44ded86
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-03-29 17:35:31 +0100

    add isFull() & isEmpty() functions & update pop() and push() functions

--- L1/PSD/stack1.c
+++ L1/PSD/stack1.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h> 
 #include <stdlib.h>
+#include <stdbool.h>
 #define MAX 4
 
 int stack_arr[MAX];
@@ -10,10 +11,27 @@ int top = -1;  // indicates that the stack is empty.
 void push(int data); // function to push data in the stack.
 int pop(); // function to pop data from the stack. 
 void print(); // function to print the data int stack.
+bool isFull(); // function to check if the stack full or not.
+bool isEmpty(); // function to check if the stack is empty or not.
+
+bool isFull() {
+  if (top == MAX - 1)
+    return false;
+  else
+    return true;
+}
+
+bool isEmpty() {
+  
+  if (top == -1)
+    return false;
+  else 
+    return true;
+}
 
 void push(int data) {
 
-  if (top == MAX - 1) {
+  if (isFull()) {
     printf("Stack overflow\n");
     return; // indicate the end of the function.
   }
@@ -25,7 +43,7 @@ void push(int data) {
 int pop() {
   int value;
   
-  if (top == -1) {
+  if (isEmpty()) {
     printf("Stack overflow");
     exit(1);
   }
