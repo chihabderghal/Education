b23bab58bb7c0a084e76f5c13f11463b77064a9c
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-03-30 02:20:48 +0100

    write pop() push() isEmpty() isFull() print() functions

--- L1/PSD/stack3.c
+++ L1/PSD/stack3.c
@@ -5,9 +5,71 @@
 #include <stdbool.h>
 #define MAX 5
 
+int stack_arr[5];
+int top = -1;
+
+bool isEmpty();
+bool isFull();
+void push(int data);
+void pop();
+void print();
+
+bool isEmpty() {
+  
+  if (top == -1)
+    return true;
+  else 
+    return false;
+}
+
+bool isFull() {
+
+  if (top == MAX - 1)
+    return true;
+  else 
+    return false;
+}
+
+void push(int data) {
+  
+  if (isFull()) {
+    pritnf("Stack overflow\n");
+    exit(1);
+  }
+
+  top++;
+  stack_arr[top] = data;
+}
+
+void pop(int *data) {
+  
+  if (isEmpty()) {
+    printf("Stack underflow\n");
+    exit(1);
+  }
+    
+    *data = stack_arrp[top];
+    top--;
+}
+
+void print() {
+  
+  if (isEmpty()) {
+    printf("Stack underflow\n");
+    exit(1);
+  }
+
+  for (int i = top; i < 0; i--) 
+    printf("|--%d--| \n", stack_arr[i]);
+
+}
+
+
+
 int main() {
 
 
- return 0;
 
+
+ return 0;
 }
