a3b7ca7fdc2188fa7e9c7d9b3d9cf20ff6f4d25d
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-03-30 01:59:00 +0100

    add the stack3 file

---
+++ L1/PSD/stack3.c
@@ -0,0 +1,13 @@
+// Program to print all the prime factors of a numebr in decending order using a stack.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 5
+
+int main() {
+
+
+ return 0;
+
+}
