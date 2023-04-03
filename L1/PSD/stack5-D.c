01f554f3c41ff1ad6e55511f2996f20cb6ec241c
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-04-03 20:41:41 +0100

    update the main

--- L1/PSD/stack5-D.c
+++ L1/PSD/stack5-D.c
@@ -43,6 +43,34 @@ void print(){
 
 int main() {
 
+  int choice, data;
+  while (1) {
+    printf("1. Push\n");
+    printf("2. Print\n");
+    printf("3. Quit\n");
+    printf("Enter your choice : ");
+    scanf("%d", &choice);
+  }
+
+  switch (choice) {
+    case 1:
+      printf("Enter the element to be pushed : ");
+      scanf("%d", &data);
+      push(data);
+      break;
+
+    case 2:
+      printf();
+      break;
+
+    case 3:
+      exit(1);
+
+    default:
+      printf("!Eror!, Type again");
+      break;
+  }
+
 
   return 0;
 }
