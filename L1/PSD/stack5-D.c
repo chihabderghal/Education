a9d9589263a2886636359a06bdc87ef6ec6185c1
Chihab Eddine Derghal <chihabeddinederghal@gmail.com>
2023-04-03 20:26:03 +0100

    add push() function

--- L1/PSD/stack5-D.c
+++ L1/PSD/stack5-D.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 #include <bool.h>
 
+void push(int data);
+
+struct node {
+  int data;
+  struct node *link;
+} *top = NULL;
+
+void push(int data) {
+  
+  node *new-node = malloc(sizeof(new-node));
+  if (new-node == NULL) {
+    printf("Stack overflow\n");
+    exit(1);
+  }
+
+  new-node->data = data;
+  new-node->link = NULL;
+
+  new-node-> top;
+  top = new-node;
+
+}
+  
 
 
 
